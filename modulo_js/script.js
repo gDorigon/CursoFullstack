4b35196b5958174177455f04b9cb034bb8557b26
Guilherme Dorigon <guidbovo@gmail.com>
2025-02-03 19:48:36 -0300

    Manipulação de datas

--- modulo_js/script.js
+++ modulo_js/script.js
@@ -1,25 +1,60 @@
-function pedir(){
-    var valor = prompt("Digite um valor de 1 a 4");
+// Criando uma data
+var data = new Date();
+console.log(data)
 
-    switch(Number(valor)){
-        case 1:
-            alert("Você escolheu suco");
-        break;
+// Pegando Apenas as horas
+console.log(data.getHours());
 
-        case 2:
-            alert("Você escolheu agua gelada");
-        break;
+// Pegando Minutos
+console.log(data.getMinutes());
 
-        case 3:
-            alert("Você escolheu sorvete");
-        break;
+// Pegando Segundos
+console.log(data.getSeconds());
 
-        case 4:
-            alert("Você escolheu garçom");
-        break;
+// Pegando milisegundos
+console.log(data.getMilliseconds());
 
-        default:
-            alert("Opção inválida!");
-        break;
-    }
-}
+console.log("===========================")
+
+// Criando uma Data
+var datanova= new Date("Janeiro 5, 2025");
+console.log(datanova)
+
+// Converter data em milisegundos
+console.log(Date.parse(datanova));
+
+// Crinando data com os milisegundos
+console.log(new Date(1736046000000));
+
+// get = pegar
+// Pegando somente o dia da data
+console.log(`Pegando somente o dia da data: ${datanova.getDate()}`);
+
+// Pegando somente dia do mês, janeiro = 0 fevereiro = 1 o Js começa contando do 0.
+console.log(`Pegando o mês: ${datanova.getMonth()}`);
+
+// Dia da semana
+console.log(`Pegando o dia da semana: ${datanova.getDay()}`); // Comça no Domingo = 0
+
+// Pegando o Ano inteiro
+console.log(`Pegando o ano: ${datanova.getFullYear()}`);
+
+// Formatando a data
+
+// Exercicio = 05/01/2025
+console.log(`Formatando data: ${datanova.getDate() + "/" + (datanova.getMonth() + 1) + "/" + datanova.getFullYear()}`);
+
+// Somar numero na data
+// Usamos o set para setar um valor
+console.log("Somar numero data: " + datanova.setDate(datanova.getDate() + 5))
+console.log(datanova);
+
+console.log("Somar numero a Hora: " + datanova.setHours(datanova.getHours() + 5))
+console.log(datanova);
+
+// get : pagar valor
+// set : setar valor
+
+// Tranformando dia semanal em segunda, terça, quarta, quinta, sexta, sabado, dommingo
+var dias = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado"]
+console.log(`Tranformando dia 05 em Domingo: ${dias[datanova.getDay()]}`);
