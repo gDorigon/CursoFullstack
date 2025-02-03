e7a2109d2cef1a42741670fa4c10579a40addd29
Guilherme Dorigon <guidbovo@gmail.com>
2025-02-03 19:23:33 -0300

    Aprendendo loop

--- modulo_js/script.js
+++ modulo_js/script.js
@@ -1,40 +1,11 @@
-var area = document.getElementById('area');
+// var x = 0;
 
-function entrar(){
-    var nome = prompt("Digite seu nome");
-
-    if(nome === "" || nome === null){
-        alert("Deu algo errado")
-        area.innerHTML = "Clique no botão para acessar..."
-    } else{
-        area.innerHTML = "Bem vindo " + nome + " ";
-
-        let botaoSair = document.createElement("button");
-        botaoSair.innerHTML = "Sair da conta";
-
-        botaoSair.onclick = sair;
-        area.appendChild(botaoSair);
-    }
-
-}
+// while(x <= 10){
+//     document.write("<br> o valor do x é: " + x);
+//     x++;
+// }
 
 
-function sair(){
-    alert("Até mais");
-    area.innerHTML = "Você saiu";
-}
-
-function mediaAluno(nota1, nota2){
-    var media = (nota1 + nota2) /2;
-
-    if(media >= 7){
-        alert("Aluno aprovado! media: " + media);
-    } else if( media < 7){
-        alert("Aluno reprovado! media: " + media);
-    }
-}
-
-function aluno(nome, curso){
-    var mensagem = "Seja bem vindo " + nome + " ao curso de " +curso;
-    console.log(mensagem);
+for(a = 0; a < 5; a++){
+    document.write("<br> o valor do x é: " + a);
 }
