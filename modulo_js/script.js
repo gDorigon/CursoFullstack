0b3f2fef5efe46b7f19e1f809a1b564f334384f7
Guilherme Dorigon <guidbovo@gmail.com>
2025-02-03 20:15:24 -0300

    calculadora imc

--- modulo_js/script.js
+++ modulo_js/script.js
@@ -1,60 +1,35 @@
-// Criando uma data
-var data = new Date();
-console.log(data)
+var peso;
+var altura;
+var resultado;
+var imc;
+var teste1;
+var final;
 
-// Pegando Apenas as horas
-console.log(data.getHours());
 
-// Pegando Minutos
-console.log(data.getMinutes());
 
-// Pegando Segundos
-console.log(data.getSeconds());
+function calcular(event){
+    event.preventDefault();
 
-// Pegando milisegundos
-console.log(data.getMilliseconds());
+    peso = document.getElementById('peso').value;
+    altura = document.getElementById('altura').value;
 
-console.log("===========================")
+    imc = peso / (altura*altura);
 
-// Criando uma Data
-var datanova= new Date("Janeiro 5, 2025");
-console.log(datanova)
+    if(imc < 17){
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '| Muito abaixo do peso';
+    }
+    if(imc >= 17 && imc < 18){
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '| Abaixo do peso';
+    }
+    if(imc >= 18 && imc < 25){
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '| Peso ideal';
+    }
+    if(imc >= 25){
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '| Acima do peso';
+    }
 
-// Converter data em milisegundos
-console.log(Date.parse(datanova));
-
-// Crinando data com os milisegundos
-console.log(new Date(1736046000000));
-
-// get = pegar
-// Pegando somente o dia da data
-console.log(`Pegando somente o dia da data: ${datanova.getDate()}`);
-
-// Pegando somente dia do mês, janeiro = 0 fevereiro = 1 o Js começa contando do 0.
-console.log(`Pegando o mês: ${datanova.getMonth()}`);
-
-// Dia da semana
-console.log(`Pegando o dia da semana: ${datanova.getDay()}`); // Comça no Domingo = 0
-
-// Pegando o Ano inteiro
-console.log(`Pegando o ano: ${datanova.getFullYear()}`);
-
-// Formatando a data
-
-// Exercicio = 05/01/2025
-console.log(`Formatando data: ${datanova.getDate() + "/" + (datanova.getMonth() + 1) + "/" + datanova.getFullYear()}`);
-
-// Somar numero na data
-// Usamos o set para setar um valor
-console.log("Somar numero data: " + datanova.setDate(datanova.getDate() + 5))
-console.log(datanova);
-
-console.log("Somar numero a Hora: " + datanova.setHours(datanova.getHours() + 5))
-console.log(datanova);
-
-// get : pagar valor
-// set : setar valor
-
-// Tranformando dia semanal em segunda, terça, quarta, quinta, sexta, sabado, dommingo
-var dias = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado"]
-console.log(`Tranformando dia 05 em Domingo: ${dias[datanova.getDay()]}`);
+}
