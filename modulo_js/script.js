27c281fc9f229920582fd63bd98e5ca0d6a15702
Guilherme Dorigon <guidbovo@gmail.com>
2025-02-04 19:32:03 -0300

    timesetInterval e timeout

--- modulo_js/script.js
+++ modulo_js/script.js
@@ -1,35 +1,11 @@
-var peso;
-var altura;
-var resultado;
-var imc;
-var teste1;
-var final;
 
-
-
-function calcular(event){
-    event.preventDefault();
-
-    peso = document.getElementById('peso').value;
-    altura = document.getElementById('altura').value;
-
-    imc = peso / (altura*altura);
-
-    if(imc < 17){
-        resultado = document.getElementById('resultado');
-        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '| Muito abaixo do peso';
-    }
-    if(imc >= 17 && imc < 18){
-        resultado = document.getElementById('resultado');
-        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '| Abaixo do peso';
-    }
-    if(imc >= 18 && imc < 25){
-        resultado = document.getElementById('resultado');
-        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '| Peso ideal';
-    }
-    if(imc >= 25){
-        resultado = document.getElementById('resultado');
-        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '| Acima do peso';
-    }
-
+function acao(){
+    document.write("Executando... <br>");
 }
+
+// var timer = setInterval(()=>{
+//     document.write("Executando!!! <br>");
+// }, 1000);
+
+
+setTimeout(acao, 3000);
