3c89e0034f029c553456a4a084d87fb108fcec7b
Guilherme Dorigon <guidbovo@gmail.com>
2025-02-08 14:17:59 -0300

    lista de objetos

--- modulo_js/script.js
+++ modulo_js/script.js
@@ -1,11 +1,25 @@
 
-function acao(){
-    document.write("Executando... <br>");
-}
-
-// var timer = setInterval(()=>{
-//     document.write("Executando!!! <br>");
-// }, 1000);
 
 
-setTimeout(acao, 3000);
+
+// let pessoa = {
+//     nome: "Matheus",
+//     idade: 26,
+//     altura: 1.85,
+//     cargo: "Programador"
+// };
+
+
+// let carro = {
+//     nome : "Golf",
+//     cor : "Branco",
+//     potencia : "140cv"
+// }
+
+let usuarios = [
+    {nome: "Guilherme", cargo: "Dev", status: "Ativo"},
+    {nome: "Jose", cargo: "Dev", status: "Ativo"},
+    {nome: "Leleco", cargo: "Dev", status: "Ativo"},
+    {nome: " Joao", cargo: "Dev", status: "Ativo"}
+
+];
