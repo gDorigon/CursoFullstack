01cc24a6f64181647d29c9be20afeed25d9b2afb
Guilherme Dorigon <guidbovo@gmail.com>
2025-02-11 19:01:52 -0300

    Código final ToDoList com JS

--- modulo_js/ToDoList/script.js
+++ modulo_js/ToDoList/script.js
@@ -4,35 +4,63 @@ let listElement = document.querySelector("#app ul");
 let inputElement = document.querySelector("#app input");
 let buttonElement = document.querySelector("#app button");
 
-let tarefas = [];
+let tarefas = JSON.parse(localStorage.getItem("@listaTarefas")) || [];
 
-function renderTarefas() {
-    listElement.innerHTML = "";
+function renderTarefas(){
+  listElement.innerHTML = "";
 
-    tarefas.map((todo) => {
-        let liElement = document.createElement("li");
-        let tarefaText = document.createTextNode(todo);
+  tarefas.map((todo)=>{
+    let liElement = document.createElement("li");
+    let tarefaText = document.createTextNode(todo);
 
-        liElement.appendChild(tarefaText);
-        listElement.appendChild(liElement);
+    let linkElement = document.createElement("a");
+    linkElement.setAttribute("href", "#");
 
-    })
+    let linkText = document.createTextNode("Excluir");
+    linkElement.appendChild(linkText);
+
+    let posicao = tarefas.indexOf(todo);
+
+    linkElement.setAttribute("onclick", `deletarTarefa(${posicao})`)
+
+    liElement.appendChild(tarefaText);
+    liElement.appendChild(linkElement);
+    listElement.appendChild(liElement);
+
+
+  })
+
 }
 
-function adicionarTarefas() {
-    if (inputElement.value === '') {
-        alert("Digite alguma tarefa");
-        return false;
-    } else {
-        let novaTarefa = inputElement.value;
+renderTarefas();
 
-        tarefas.push(novaTarefa);
-        inputElement.value = '';
+function adicionarTarefas(){
+  if(inputElement.value === ''){
+    alert("Digite alguma tarefa");
+    return false;
+  }else{
+    let novaTarefa = inputElement.value;
 
-        renderTarefas();
+    tarefas.push(novaTarefa);
+    inputElement.value = '';
 
-    }
+    renderTarefas();
+    salvarDados();
+
+  }
 
 }
 
 buttonElement.onclick = adicionarTarefas;
+
+
+function deletarTarefa(posicao){
+  tarefas.splice(posicao, 1);
+  renderTarefas();
+  salvarDados();
+}
+
+
+function salvarDados(){
+  localStorage.setItem("@listaTarefas", JSON.stringify(tarefas) )
+}
