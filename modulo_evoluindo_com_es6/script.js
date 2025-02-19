276d6f3eed7835c9d826e32fdda6c4852ba6a197
Guilherme Dorigon <guidbovo@gmail.com>
2025-02-19 19:56:59 -0300

    Desafio 3 do final do módulo ES6

--- modulo_evoluindo_com_es6/script.js
+++ modulo_evoluindo_com_es6/script.js
@@ -1,15 +1,16 @@
-// 2) Desafio da Verificação de Elemento em um Array: Crie uma função que deve esperar receber 2 parâmetros, primeiro deve ser um array de números e o segundo parâmetro é um numero que deseja conferir se esse numero existe dentro do array e com isso você deve mostrar o resultado se esse numero que colocou existe dentro do seu array de números. 
+// 3) Com base no array abaixo o desafio como você pode percorrer e encontrar o produto com Preço Igual a R$20 e o resultado deve mostrar apenas o produto que tem o preço igual a R$20:​
 
-let array = [1,2,3,4,5,6,7,9,10, 20, 30, 40, 50, 60, 70 ,80, 90, 100];
+const products = [ { name: 'Maça', price: 2.5 }, { name: 'Coca cola', price: 8 }, { name: 'Guarana', price: 5 }, { name: 'Chocolate', price: 20 } ];
 
 
-function verificaNumero(arrayDeItens, Localizador){
-    if(arrayDeItens.includes(Localizador)){
-        return console.log("Possui o número");
+function produtoEncontrado() {
+    const produto = products.find(produto => produto.price === 20);
+    if (produto) {
+        console.log(produto);
     } else {
-        return console.log("Não possui o número");
+        console.log("Nenhum produto com preço R$20 encontrado.");
     }
 }
 
+produtoEncontrado();
 
-verificaNumero(array, 51);
