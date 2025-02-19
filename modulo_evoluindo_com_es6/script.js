5f329a58419ea62f1f56e6a1080b7c9e456a30c9
Guilherme Dorigon <guidbovo@gmail.com>
2025-02-19 19:37:57 -0300

    Desafio 2 do final do módulo ES6

--- modulo_evoluindo_com_es6/script.js
+++ modulo_evoluindo_com_es6/script.js
@@ -1,18 +1,15 @@
-// ​1) Verificador de Número Negativo, Positivo ou Zero: Escreva uma função que recebe um número como parâmetro e verifica se é negativo, positivo ou igual a zero. A função deve retornar uma string indicando o resultado.
+// 2) Desafio da Verificação de Elemento em um Array: Crie uma função que deve esperar receber 2 parâmetros, primeiro deve ser um array de números e o segundo parâmetro é um numero que deseja conferir se esse numero existe dentro do array e com isso você deve mostrar o resultado se esse numero que colocou existe dentro do seu array de números. 
 
-let numero = 0;
+let array = [1,2,3,4,5,6,7,9,10, 20, 30, 40, 50, 60, 70 ,80, 90, 100];
 
-function verificaNumero(numero){
 
-    if(numero > 0){
-        console.log("Número positivo");
-    } else if(numero === 0){
-        console.log("Número é 0");
+function verificaNumero(arrayDeItens, Localizador){
+    if(arrayDeItens.includes(Localizador)){
+        return console.log("Possui o número");
     } else {
-        console.log("Número negativo");
+        return console.log("Não possui o número");
     }
-
 }
 
 
-verificaNumero(0);
+verificaNumero(array, 51);
