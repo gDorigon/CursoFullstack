0ad9d32e8f4cc25850c259dfbe73b69d16a8edbd
Guilherme Dorigon <guidbovo@gmail.com>
2025-02-22 08:56:44 -0300

    Inicio módulo react aula 2, criando componentes

--- modulo_react/primeiro-app/src/App.js
+++ modulo_react/primeiro-app/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Nome from './components/Nome'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  return(
+    <div>
+      <h1>Componente App</h1>
+      <Nome aluno="Guilherme" idade={30} />
+      <br/>
+      <Nome aluno="Eduardo" idade={33}/>
     </div>
   );
 }
