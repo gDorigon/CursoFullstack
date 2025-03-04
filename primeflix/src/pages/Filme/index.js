b7ff6fadfa64ffc48c7062053b0e52458b0d2ab2
Guilherme Dorigon <guidbovo@gmail.com>
2025-03-04 13:12:31 -0300

    Estilizando página sobre o filme

--- primeflix/src/pages/Filme/index.js
+++ primeflix/src/pages/Filme/index.js
@@ -1,6 +1,7 @@
 import {useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
 import api from '../../services/api';
+import './filme-info.css'
 
 
 function Filme(){
@@ -52,6 +53,18 @@ function Filme(){
             <span>{filme.overview}</span>
 
             <strong> Avaliação: {filme.vote_average}/10</strong>
+
+            <div className='area-buttons'>
+
+                <button> Salvar </button>
+
+                <button>
+                    <a href='#'>
+                        Trailer
+                    </a>
+                </button>
+
+            </div>
         </div>
     )
 }
