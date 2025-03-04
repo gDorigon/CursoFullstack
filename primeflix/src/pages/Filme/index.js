59749458caf3bd09286cf25a97374c4715da2d14
Guilherme Dorigon <guidbovo@gmail.com>
2025-03-04 14:23:29 -0300

    Projeto concluido + notificações com toastfy

--- primeflix/src/pages/Filme/index.js
+++ primeflix/src/pages/Filme/index.js
@@ -2,6 +2,7 @@ import {useEffect, useState} from 'react';
 import { useParams, useNavigate} from 'react-router-dom';
 import api from '../../services/api';
 import './filme-info.css'
+import { toast } from 'react-toastify'
 
 
 function Filme(){
@@ -45,13 +46,13 @@ function Filme(){
         const hasFilme = filmesSalvos.some((filmesSalvo)=>filmesSalvo.id === filme.id);
 
         if(hasFilme){
-            alert("Esse filme já está na lista");
+            toast.warn("Filme já está na lista");
             return;
         }
 
         filmesSalvos.push(filme);
         localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
-        alert("FILME SALVO");
+        toast.success("Filme salvo com sucesso");
     }
 
 
