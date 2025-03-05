97ed9aeed39c0bad5f5472d00d0f38fcacb5ace3
Guilherme Dorigon <guidbovo@gmail.com>
2025-03-05 19:40:28 -0300

    criando e lendo database

---
+++ modulo_firebase/src/firebaseConnection.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore} from 'firebase/firestore'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBrqjvBk9fggrHpd02vRssIMZUJFfwXC6M",
+    authDomain: "curso-91ae7.firebaseapp.com",
+    projectId: "curso-91ae7",
+    storageBucket: "curso-91ae7.firebasestorage.app",
+    messagingSenderId: "753479428394",
+    appId: "1:753479428394:web:2ca321c84545d7a52c9653",
+    measurementId: "G-FTNNNQY7XN"
+  };
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp)
+
+export{db};
