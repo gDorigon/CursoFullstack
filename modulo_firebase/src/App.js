217abfd4ec676eb89b4f600d23f1a4e85e91acc2
Guilherme Dorigon <guidbovo@gmail.com>
2025-03-06 11:18:47 -0300

    atualizando em tempo real com useEfect

--- modulo_firebase/src/App.js
+++ modulo_firebase/src/App.js
@@ -1,6 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { db } from './firebaseConnection';
-import { doc, setDoc, collection, addDoc, getDoc, getDocs, updateDoc, deleteDoc } from 'firebase/firestore'
+import { doc, setDoc, collection, addDoc, getDoc, getDocs, updateDoc, deleteDoc, onSnapshot } from 'firebase/firestore'
 
 import './app.css';
 
@@ -8,8 +8,27 @@ function App() {
   const [titulo, setTitulo] = useState('');
   const [autor, setAutor] = useState('');
   const [idPost, setIdPost] = useState('');
-
   const [posts, setPosts] = useState([]);
+
+  useEffect(()=>{
+    async function loadPosts() {
+      const unsub = onSnapshot(collection(db, "posts"),(snapshot)=>{
+        let listaPost = [];
+
+        snapshot.forEach((doc) => {
+          listaPost.push({
+            id: doc.id,
+            titulo: doc.data().titulo,
+            autor: doc.data().autor,
+          })
+        })
+
+        setPosts(listaPost);
+      })
+    }
+
+    loadPosts();
+  }, [])
 
 
   async function handleAdd() {
