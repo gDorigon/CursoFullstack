e7989d5cac5924d79c127ff0feca9c398527220b
Guilherme Dorigon <guidbovo@gmail.com>
2025-03-12 20:23:19 -0300

    Type Number - typescript

---
+++ modulo-typescript1/build/type_number.js
@@ -0,0 +1,5 @@
+"use strict";
+let valor1 = 10;
+valor1 = valor1 + 20.5;
+let valor2 = 15.63;
+console.log(valor1 + valor2);
---
+++ modulo-typescript1/src/type_number.ts
@@ -0,0 +1,11 @@
+
+// number = int, float, hex, binario
+
+let valor1: number = 10;
+
+valor1 = valor1 + 20.5;
+
+let valor2: number = 15.63;
+
+
+console.log(valor1 + valor2)
