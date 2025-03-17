7f3939ad4651437fd55884f92f15dca88bcbb086
Guilherme Dorigon <guidbovo@gmail.com>
2025-03-17 12:40:00 -0300

    type_tupla

---
+++ modulo-typescript1/build/type_tupla.js
@@ -0,0 +1,6 @@
+"use strict";
+let aluno;
+aluno = ["Guilherem Dorigon", 21];
+aluno.push("Matheus", 4321);
+aluno.push(123, "Lucas");
+console.log(aluno);
---
+++ modulo-typescript1/src/type_tupla.ts
@@ -0,0 +1,11 @@
+// tupla existe apenas no TS
+
+let aluno: [string, number];
+
+aluno = ["Guilherem Dorigon", 21];
+
+aluno.push("Matheus", 4321)
+
+aluno.push(123, "Lucas");
+
+console.log(aluno);
