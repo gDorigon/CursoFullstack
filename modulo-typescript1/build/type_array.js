56c9f40e99e7b5b51e28f23e85cf7d3d581a950a
Guilherme Dorigon <guidbovo@gmail.com>
2025-03-17 12:24:27 -0300

    type array - typescript

---
+++ modulo-typescript1/build/type_array.js
@@ -0,0 +1,8 @@
+"use strict";
+let numeros;
+let filmes;
+filmes = ["Filme1", "Filme2", "Filme3"];
+filmes.push(100);
+console.log("Meus filmes: ", filmes);
+numeros = [1, 5, 21, 30];
+numeros.push(55);
---
+++ modulo-typescript1/src/type_array.ts
@@ -0,0 +1,16 @@
+
+
+let numeros:number[];
+
+let filmes: Array<string | number>;
+
+filmes = ["Filme1", "Filme2", "Filme3"];
+
+filmes.push(100);
+
+console.log("Meus filmes: ",filmes)
+
+numeros = [1,5,21,30];
+
+numeros.push(55);
+
