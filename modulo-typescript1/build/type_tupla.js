803dec8218ec1869708364de97b54dda8f5e056f
Guilherme Dorigon <guidbovo@gmail.com>
2025-03-17 21:03:51 -0300

    type tupla - typescript

--- modulo-typescript1/build/type_tupla.js
+++ modulo-typescript1/build/type_tupla.js
@@ -1,6 +1,4 @@
 "use strict";
-let aluno;
-aluno = ["Guilherem Dorigon", 21];
-aluno.push("Matheus", 4321);
-aluno.push(123, "Lucas");
-console.log(aluno);
+let statusPedido;
+statusPedido = ["Em produção", "Saiu para entrega", "Pedido entregue"];
+console.log(statusPedido);
--- modulo-typescript1/src/type_tupla.ts
+++ modulo-typescript1/src/type_tupla.ts
@@ -1,11 +1,17 @@
 // tupla existe apenas no TS
 
-let aluno: [string, number];
+// let aluno: [string, number];
 
-aluno = ["Guilherem Dorigon", 21];
+// aluno = ["Guilherem Dorigon", 21];
 
-aluno.push("Matheus", 4321)
+// aluno.push("Matheus", 4321);
 
-aluno.push(123, "Lucas");
+// aluno.push(123, "Lucas");
 
-console.log(aluno);
+// console.log(aluno);
+
+let statusPedido: [string, string, string];
+
+statusPedido = ["Em produção", "Saiu para entrega", "Pedido entregue"];
+
+console.log(statusPedido);
