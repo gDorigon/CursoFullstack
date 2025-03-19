e5b327d09255252d97db54139b95a6ea3c70f207
Guilherme Dorigon <guidbovo@gmail.com>
2025-03-19 20:36:25 -0300

    arrow functions

---
+++ modulo-typescript1/src/functions/arrowfunctions.ts
@@ -0,0 +1,12 @@
+
+
+// function retornoAPI(url: string): string{
+//      return url;
+// }
+
+const retornoAPI = (url: string): void => {
+
+    console.log('URL da API: ', url)
+}
+
+retornoAPI("https://sujeitoprogramador.com")
