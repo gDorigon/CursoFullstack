a197ebc43408ab1d8afe1e2b0dd35a2ec3549756
Guilherme Dorigon <guidbovo@gmail.com>
2025-03-22 16:56:44 -0300

    metodo Protected

--- modulo_typescript3/build/eranca.js
+++ modulo_typescript3/build/eranca.js
@@ -1,22 +1,26 @@
 "use strict";
 // 4 Modificadores: public, private, protected, readonly
 class Usuario {
-    constructor(nome, email) {
+    constructor(id, nome, email) {
         this.nome = nome;
         this.email = email;
+        this.id = id;
     }
-    mudarCargo() {
-        console.log("Alterando cargo...");
+    mudarCargo(cargo) {
+        console.log("Alterando cargo para: ", cargo);
+        console.log("USER ID:", this.id);
+    }
+    acessarAdmin() {
+        this.mudarCargo("Designer");
     }
 }
 class Admin extends Usuario {
-    constructor(nome, email, cargo, nivel) {
-        super(nome, email);
+    constructor(id, nome, email, cargo, nivel) {
+        super(id, nome, email);
         this.cargo = cargo;
         this.nivel = nivel;
     }
 }
-const usuario1 = new Admin("Guilherme", "guilherme@teste.com", "Programador", 2);
-usuario1.cargo = "Designer";
-usuario1.mudarCargo();
+const usuario1 = new Admin(10, "Guilherme", "guilherme@teste.com", "Programador", 2);
 console.log(usuario1);
+usuario1.acessarAdmin();
--- modulo_typescript3/src/eranca.ts
+++ modulo_typescript3/src/eranca.ts
@@ -1,38 +1,43 @@
 
 // 4 Modificadores: public, private, protected, readonly
 
-class Usuario{
-    public nome: string;
-    public email: string;
+class Usuario {
+    protected id: number; // podemos chamar na classe pai ou classes que extende a classe usuario ( filhas )
+    nome: string;
+    email: string;
 
-    constructor(nome: string, email: string){
+    constructor(id: number, nome: string, email: string) {
         this.nome = nome;
         this.email = email;
+        this.id = id;
     }
 
-    mudarCargo(): void {
-        console.log("Alterando cargo...")
+    protected mudarCargo(cargo: string): void {
+        console.log("Alterando cargo para: ", cargo)
+        console.log("USER ID:", this.id);
+    }
+
+    acessarAdmin(){
+       this.mudarCargo("Designer"); 
     }
 }
 
-class Admin extends Usuario{
+class Admin extends Usuario {
     cargo: string;
     nivel: number;
 
-    constructor(nome: string, email: string, cargo: string, nivel: number){
+    constructor(id: number, nome: string, email: string, cargo: string, nivel: number) {
 
-        super(nome, email);
+        super(id, nome, email);
 
         this.cargo = cargo;
         this.nivel = nivel;
     }
 }
 
-const usuario1 = new Admin("Guilherme", "guilherme@teste.com", "Programador", 2);
-
-usuario1.cargo = "Designer";
-
-usuario1.mudarCargo();
+const usuario1 = new Admin(10, "Guilherme", "guilherme@teste.com", "Programador", 2);
 
 console.log(usuario1)
+
+usuario1.acessarAdmin();
 
