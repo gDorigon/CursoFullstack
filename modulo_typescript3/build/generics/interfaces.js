5a98c9c9203d4d47a512eb5b1389ec0fc671cb41
Guilherme Dorigon <guidbovo@gmail.com>
2025-03-27 18:02:03 -0300

    Generic em interfaces

---
+++ modulo_typescript3/build/generics/interfaces.js
@@ -0,0 +1,18 @@
+"use strict";
+// type NovoProduto<V extends number | string> = {
+//     nome: string;
+//     preco: V;
+// }
+const arroz = {
+    nome: "Arroz Branco",
+    preco: 12,
+    formatar(valor) {
+        let valorFormatado = valor.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        });
+        return valorFormatado;
+    }
+};
+console.log(arroz);
+console.log(arroz.formatar(1500));
