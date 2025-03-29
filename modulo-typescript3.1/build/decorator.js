dd9661a16f7ea74c268ee4241a50bf225b9c7067
Guilherme Dorigon <guidbovo@gmail.com>
2025-03-29 10:30:26 -0300

    Praticando decorators

--- modulo-typescript3.1/build/decorator.js
+++ modulo-typescript3.1/build/decorator.js
@@ -9,13 +9,29 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function logInfo(mensagem) {
+// function logInfo(mensagem: string){
+//     return (target: any)=>{
+//         console.log(`${mensagem}, ${target}`)
+//     }
+// }
+// @logInfo("Servidor está rodando")
+// class Sistema{
+// }
+// ================ Praticando
+function setIpServidor(novoIp) {
     return (target) => {
-        console.log(`${mensagem}, ${target}`);
+        return class extends target {
+            constructor() {
+                super(...arguments);
+                this.ip = novoIp;
+            }
+        };
     };
 }
-let Sistema = class Sistema {
+let Servidor = class Servidor {
 };
-Sistema = __decorate([
-    logInfo("Servidor está rodando")
-], Sistema);
+Servidor = __decorate([
+    setIpServidor("192.168.12.10")
+], Servidor);
+const servidor1 = new Servidor();
+console.log(servidor1);
--- modulo-typescript3.1/src/decorator.ts
+++ modulo-typescript3.1/src/decorator.ts
@@ -5,16 +5,36 @@
 // }
 
 
-function logInfo(mensagem: string){
+// function logInfo(mensagem: string){
 
-    return (target: any)=>{
-        console.log(`${mensagem}, ${target}`)
+//     return (target: any)=>{
+//         console.log(`${mensagem}, ${target}`)
+//     }
+// }
+
+
+// @logInfo("Servidor está rodando")
+// class Sistema{
+
+// }
+
+// ================ Praticando
+
+function setIpServidor(novoIp: string){
+    return (target: any) => { // target = é o construtor da classe
+        return class extends target{
+            ip = novoIp
+        }
     }
 }
 
-
-@logInfo("Servidor está rodando")
-class Sistema{
+@setIpServidor("192.168.12.10")
+class Servidor{
 
 }
+
+
+const servidor1 = new Servidor();
+
+console.log(servidor1);
 
