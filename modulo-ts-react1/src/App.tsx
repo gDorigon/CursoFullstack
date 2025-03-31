c15d8b6e83e0d4051c6d72b24530825b89290e7b
Guilherme Dorigon <guidbovo@gmail.com>
2025-03-31 20:41:09 -0300

    Aplicando useState

--- modulo-ts-react1/src/App.tsx
+++ modulo-ts-react1/src/App.tsx
@@ -1,19 +1,44 @@
-import { Header } from './components/header'
-import { Aluno } from './components/aluno'
-import { Footer } from './components/footer'
+import { useState } from "react"
 
 
 export default function App() {
+
+  const [input, setInput] = useState("")
+  const [idade, setIdade] = useState("")
+  const [aluno, setAluno] = useState("")
+
+
+  function mostrarAluno() {
+    setAluno(input)
+    console.log(idade)
+  }
   return (
     <div>
-      <Header title='Dorigoat do React' />
-      <Aluno nome="Leticya" idade={20} />
+      <h1> Conhecendo useState</h1>
 
-      <Aluno nome="Guilherme" idade={21} />
+      <input
+        placeholder="Digite o nome"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
 
-      <Aluno nome="Dorigon" idade={30} />
+      <br />
+      <br />
+      <input
+        placeholder="Digite sua idade"
+        value={idade}
+        onChange={(e) => setIdade(e.target.value)}
 
-      <Footer />
+      />
+
+      <br />
+      <br />
+
+      <button onClick={mostrarAluno}> Mostrar aluno </button>
+
+      <hr />
+
+      <h3> Bem vindo: {aluno}</h3>
     </div>
 
   )
--- modulo-ts-react1/src/components/footer.tsx
+++
@@ -1,7 +0,0 @@
-export function Footer() {
-    return (
-        <footer>
-            <h4> Todos direitos reverdaos - @curso</h4>
-        </footer>
-    )
-}
