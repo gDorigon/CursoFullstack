44be30bcdeae2f004dd1224138f4e55afde739f1
Guilherme Dorigon <guidbovo@gmail.com>
2025-03-31 20:12:27 -0300

    (formatando doc)

--- modulo-ts-react1/src/App.tsx
+++ modulo-ts-react1/src/App.tsx
@@ -1,24 +1,24 @@
-export default function App(){
-  return(
+export default function App() {
+  return (
     <div>
       <h1> Meu projeto </h1>
-      <Aluno nome="Leticya" idade={20}/>
+      <Aluno nome="Leticya" idade={20} />
 
-      <Aluno nome="Guilherme" idade={21}/>
+      <Aluno nome="Guilherme" idade={21} />
 
-      <Aluno nome="Dorigon" idade={30}/>
+      <Aluno nome="Dorigon" idade={30} />
     </div>
 
   )
 }
 
-interface AlunoProps{
+interface AlunoProps {
   nome: string;
   idade: number;
 }
 
-function Aluno({nome, idade} : AlunoProps){
-  return(
+function Aluno({ nome, idade }: AlunoProps) {
+  return (
     <div>
       <h1> Aluno(a): {nome}</h1>
       <h3> Idade: {idade}</h3>
