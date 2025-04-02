8d09256eeaf0bb090a19de4e2033845720b86c21
Guilherme Dorigon <guidbovo@gmail.com>
2025-04-01 21:27:04 -0300

    Aplicando useState pt 2

--- modulo-ts-react1/src/App.tsx
+++ modulo-ts-react1/src/App.tsx
@@ -1,17 +1,42 @@
 import { useState } from "react"
 
 
+interface InfoAlunoProps {
+  nome: string;
+  idade: string;
+}
+
 export default function App() {
 
   const [input, setInput] = useState("")
   const [idade, setIdade] = useState("")
-  const [aluno, setAluno] = useState("")
+
+  const [infoAluno, setInfoAluno] = useState<InfoAlunoProps>()
+
+  const [contador, setContador] = useState(0)
 
 
   function mostrarAluno() {
-    setAluno(input)
-    console.log(idade)
+
+    setInfoAluno({
+      nome: input,
+      idade: idade,
+    })
+
   }
+
+  function adicionar() {
+    setContador(valorAtual => valorAtual + 1)
+  }
+
+  function diminuir() {
+    if (contador === 0) {
+      return
+    }
+    setContador(valorAtual => valorAtual - 1)
+  }
+
+
   return (
     <div>
       <h1> Conhecendo useState</h1>
@@ -38,7 +63,16 @@ export default function App() {
 
       <hr />
 
-      <h3> Bem vindo: {aluno}</h3>
+      <h3> Bem vindo: {infoAluno?.nome}</h3>
+      <h4> Idade: {infoAluno?.idade}</h4>
+
+      <hr />
+
+      <br />
+      <br />
+      <h1> Contador com useState</h1>
+
+      <button onClick={adicionar}> + </button> {contador} <button onClick={diminuir}> - </button>
     </div>
 
   )
