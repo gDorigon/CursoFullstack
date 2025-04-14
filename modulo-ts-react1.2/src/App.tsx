8216be2d68616b36fae28bea07ca98ac74f7a00e
Guilherme Dorigon <guidbovo@gmail.com>
2025-04-14 19:31:43 -0300

    Projeto Frases

--- modulo-ts-react1.2/src/App.tsx
+++ modulo-ts-react1.2/src/App.tsx
@@ -1,14 +1,27 @@
 import { useState } from 'react'
 import './App.css'
+import logoImg from './assets/logo.png'
 
 function App() {
 
-  const [userName, setUserName] = useState("matheusfraga")
-
   return (
-    <div>
-      {userName.length >= 5 && <h1>Username muito grande </h1>}
-    </div>
+    <div className='container'>
+      <img src={logoImg} alt="logo" className='logo' />
+
+      <h2 className='title'>Categorias</h2>
+
+      <section className='category-area'>
+
+        <button className='category-button'>Motivaçãoa</button>
+
+        <button className='category-button'>Bem estar</button>
+
+      </section>
+
+      <button className='button-frase'>Gerar frase</button>
+
+      <p className='texto-frase'>Alguma frase vai vir aqui</p>
+    </ div>
   )
 }
 
