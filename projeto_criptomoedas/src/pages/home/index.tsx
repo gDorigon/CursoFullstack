c703148d18d03f5f7d08eee1daacc6b51d3675ab
Guilherme Dorigon <guidbovo@gmail.com>
2025-04-26 15:26:47 -0300

    04 - Projeto Criptomoedas
    Inserindo API e listando moedas

--- projeto_criptomoedas/src/pages/home/index.tsx
+++ projeto_criptomoedas/src/pages/home/index.tsx
@@ -1,15 +1,96 @@
 import styles from './home.module.css'
 import { BsSearch } from 'react-icons/bs'
-import { Link } from 'react-router'
+import { data, Link, useNavigate } from 'react-router'
+import { useState, FormEvent, useEffect } from 'react'
+
+interface CoinProps {
+    id: string,
+    name: string,
+    symbol: string,
+    priceUsd: string,
+    vwap24Hr: string,
+    changePercent24Hr: string,
+    rank: string,
+    supply: string,
+    maxSupply: string,
+    marketCapUsd: string,
+    volumeUsd24Hr: string,
+    explorer: string,
+    formatedPrice?: string,
+    formatedMarket?: string,
+    formatedVolume?: string;
+}
+
+interface DataProps {
+    data: CoinProps[]
+}
 
 export function Home() {
+    const [input, setInput] = useState("")
+
+    const [coins, setCoins] = useState<CoinProps[]>([]);
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    async function getData() {
+        fetch("https://rest.coincap.io/v3/assets?limit=10&offset=0&apiKey=ed4c4bfb2f9dc7ad8fddc0d1b94296fe93c8a0ec77a6195537fc6f76da2beb82")
+            .then(response => response.json())
+            .then((data: DataProps) => {
+                const coinsData = data.data;
+                const price = Intl.NumberFormat("en-US", {
+                    style: "currency",
+                    currency: "USD"
+                })
+
+                const priceCompact = Intl.NumberFormat("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                    notation: "compact"
+                })
+
+                const formatedResult = coinsData.map((item) => {
+                    const formated = {
+                        ...item,
+                        formatedPrice: price.format(Number(item.priceUsd)),
+                        formatedMarket: priceCompact.format(Number(item.marketCapUsd)),
+                        formatedVolume: priceCompact.format(Number(item.volumeUsd24Hr))
+                    }
+
+                    return formated;
+                })
+
+                setCoins(formatedResult);
+
+            })
+
+    }
+
+    const navigate = useNavigate();
+
+
+
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault();
+
+        if (input === "") return
+
+        navigate(`/detail/${input}`)
+    }
+
+    function handleGetMore() {
+
+    }
 
     return (
         <main className={styles.container}>
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={handleSubmit}>
                 <input
                     placeholder='Digite o nome da moeda... Ex: Bitcoin'
                     type='text'
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
                 />
                 <button type='submit'>
                     <BsSearch size={30} color='#FFF' />
@@ -29,37 +110,50 @@ export function Home() {
                 </thead>
 
                 <tbody id='tbody'>
-                    <tr className={styles.tr}>
 
-                        <td className={styles.tdLabel} data-label='Moeda'>
-                            <div className={styles.name}>
-                                <Link to={'/detail/bitcoin'}>
-                                    <span> Bitcoin </span> | BTC
-                                </Link>
-                            </div>
-                        </td>
+                    {coins.length > 0 && coins.map((item) => (
+                        <tr className={styles.tr} key={item.id}>
 
-                        <td className={styles.tdLabel} data-label='Valor mercado'>
-                            1T
-                        </td>
+                            <td className={styles.tdLabel} data-label='Moeda'>
+                                <div className={styles.name}>
+                                    <img
+                                        className={styles.logo}
+                                        alt='Logo Cripto'
+                                        src={`https://assets.coincap.io/assets/icons/${item.symbol.toLowerCase()}@2x.png`}
+                                    />
+                                    <Link to={`/detail/${item.id}`}>
+                                        <span> {item.name} </span> | {item.symbol}
+                                    </Link>
+                                </div>
+                            </td>
 
-                        <td className={styles.tdLabel} data-label='Preço'>
-                            8.000
-                        </td>
+                            <td className={styles.tdLabel} data-label='Valor mercado'>
+                                {item.formatedMarket}
+                            </td>
 
-                        <td className={styles.tdLabel} data-label='Volume'>
-                            2B
-                        </td>
+                            <td className={styles.tdLabel} data-label='Preço'>
+                                {item.formatedPrice}
+                            </td>
 
-                        <td className={styles.tdProfit} data-label='Mudança 24h'>
-                            1.20
-                        </td>
+                            <td className={styles.tdLabel} data-label='Volume'>
+                                {item.formatedVolume}
+                            </td>
 
-                    </tr>
+                            <td className={Number(item.changePercent24Hr) > 0 ? styles.tdProfit : styles.tdLoss} data-label='Mudança 24h'>
+
+                                <span>{Number(item.changePercent24Hr).toFixed(2)}</span>
+
+                            </td>
+
+                        </tr>
+                    ))}
+
                 </tbody>
             </table>
 
-
+            <button className={styles.buttonMore} onClick={handleGetMore}>
+                Carregar mais
+            </button>
 
 
 
