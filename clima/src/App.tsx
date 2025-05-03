3172bfaad5d4bfb52afbb5587372aca39b04feb0
Guilherme Dorigon <guidbovo@gmail.com>
2025-05-03 12:03:45 -0300

    Update App.tsx

--- clima/src/App.tsx
+++ clima/src/App.tsx
@@ -2,56 +2,56 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
-  const [input, setInput] = useState("")
+  const [input, setInput] = useState("");
+  const [dadosClima, setDadosClima] = useState<any>(null);
 
   function handleBuscarCidade(cidade: string) {
-
-    const apiKey = 'd9b86d73fea820529335083254eb6fa4'; // Substitua pela sua chave de API
-    // Definindo o código do país para refinar a busca
+    const apiKey = 'd9b86d73fea820529335083254eb6fa4';
     const countryCode = 'BR';
 
-    // Função assíncrona para obter os dados do clima da API
     async function getWeather() {
-      // Construindo a URL da API com os parâmetros necessários
       const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
       const url = `${baseUrl}?q=${cidade},${countryCode}&appid=${apiKey}&units=metric&lang=pt`;
 
       try {
-        // Fazendo uma requisição à API usando fetch
         const response = await fetch(url);
-
-        // Verificando se a resposta não foi bem-sucedida
         if (!response.ok) {
-          // Lançando um erro com a mensagem da resposta
           throw new Error(`Erro: ${response.statusText}`);
         }
 
-        // Convertendo a resposta em JSON
         const data = await response.json();
-
-        // Atualizando as informações do clima na págin
+        setDadosClima(data);
       } catch (error) {
-        alert("ERRO")
+        alert("Cidade não encontrada.");
+        setDadosClima(null);
       }
     }
 
+    getWeather();
   }
-
 
   return (
     <div>
       <h1>App ClimaTempo</h1>
 
-      <input type="text"
-        placeholder='Insira a cidade que deseja buscar'
+      <input
+        type="text"
+        placeholder="Insira a cidade que deseja buscar"
         value={input}
-        onChange={(e) => { setInput(e.target.value) }} />
+        onChange={(e) => setInput(e.target.value)}
+      />
 
       <button onClick={() => handleBuscarCidade(input)}>Buscar</button>
 
+      {dadosClima && (
+        <div>
+          <h2>{dadosClima.name}</h2>
+          <p>{dadosClima.main.temp}°C</p>
+          <p>{dadosClima.weather[0].description}</p>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
