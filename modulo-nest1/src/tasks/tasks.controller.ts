5f7f8d3e4fdcfdcd1b34704e9c2b6b47b6c57392
Guilherme Dorigon <guidbovo@gmail.com>
2025-05-29 15:49:14 -0300

    Rotas dinamicas e query params

--- modulo-nest1/src/tasks/tasks.controller.ts
+++ modulo-nest1/src/tasks/tasks.controller.ts
@@ -1,18 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
-  constructor(private readonly taskService: TasksService){}
+   constructor(private readonly taskService: TasksService) { }
 
-  @Get()
-  getTasks() {
-    return this.taskService.listAllTasks()
-  }
+   @Get()
+   findAllTasks(@Query() queryParam: any) {
+      return this.taskService.findAll()
+   }
 
-  @Get("/1")
-  getTest(){
-    return this.taskService.findOneTask();
-  }
+   @Get(":id")
+   findOneTask(@Param('id') id: string) {
+      console.log(id);
+      return this.taskService.findOne(id);
+   }
 
 }
--- modulo-nest1/src/tasks/tasks.service.ts
+++ modulo-nest1/src/tasks/tasks.service.ts
@@ -3,14 +3,14 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class TasksService {
 
-  listAllTasks(){
-    return [
-      { id: 1, task: "Comprar Pão" }
-    ]
-  }
+   findAll() {
+      return [
+         { id: 1, task: "Comprar Pão" }
+      ]
+   }
 
-  findOneTask(){
-    return "Tarefa Matheus Teste..."
-  }
+   findOne(id: string) {
+      return "Buscar tarefa com ID: " + id
+   }
 
 }
